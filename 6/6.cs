8ce0fb631946d7e26f70222b038388b69a927cf2
sunrowsforus <sunrowsforus@mail.ru>
2022-10-20 18:08:18 +0300

    Круто у нас получается

---
+++ 6/6.cs
@@ -0,0 +1,13 @@
+﻿//Задача 6-Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+System.Console.WriteLine("Введите число для проверки четности: ");
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = num1 % 2;
+if (num2 == 0)
+{
+    System.Console.WriteLine($"Число {num1}, четный!");
+}
+else
+{
+    System.Console.WriteLine($"Число {num1}, нечетный!");
+}
