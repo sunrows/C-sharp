30b452c58020bdfa7ad140a25fc829b0958cfbe9
sunrowsforus <sunrowsforus@mail.ru>
2022-10-31 22:08:05 +0300

    Добавлены новые папки с проверенными задачами

---
+++ 21/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 21 Напишите программу которая принимает на вход координаты двух точек и находит расстояние между ними в 3д
+//например А(3,6,8); B(2,1,-7) ; 15.84
+
+double Rasstoyanie (int x0, int y0, int x1, int y1, int z0, int z1)
+{
+    return Math.Sqrt(Math.Pow((x0-x1),2)+Math.Pow((y0-y1),2)+Math.Pow((z0-z1),2));
+}
+System.Console.WriteLine($"Введите координаты точек в новых строках X0,Y0,X1,Y1,Z0,Z1:");
+
+int X0 = int.Parse(Console.ReadLine()!);
+int Y0 = int.Parse(Console.ReadLine()!);
+int X1 = int.Parse(Console.ReadLine()!);
+int Y1 = int.Parse(Console.ReadLine()!);
+int Z0 = int.Parse(Console.ReadLine()!);
+int Z1 = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine($"Расстояние между точек: ({X0},{Y0})({X1},{Y1})({Z0},{Z1}): {Rasstoyanie(X0,Y0,X1,Y1,Z0,Z1)}");
+
---
+++ 23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23- Задать число / и до этого числа все числа возводит в куб,
+System.Console.WriteLine("Введите предел числа для возведения их в куб: ");
+int num1 = int.Parse(Console.ReadLine()!);
+int i = 1;
+double Kub(double x)
+{
+    double result = Math.Pow(x,3);
+    return result;
+}
+while (i <= num1)
+{
+    System.Console.WriteLine($"{i}^3: {Kub(i)}");
+    i++;
+}
+
+
