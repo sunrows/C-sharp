ac7b685b986eeb91a78a811f8e2299017fa77bd4
sunrowsforus <sunrowsforus@mail.ru>
2022-11-10 18:38:54 +0300

    Домашняя работа к уроку добавилась

---
+++ 25/25.cs
@@ -0,0 +1,16 @@
+﻿//Задача 25: напишем цикл, который принимает на вход 2 числа (а и b) и 
+//возводит число a на b т.е. a^b=c 3^5=243
+
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+int i = 0;
+int res = 1;
+while (i < b)
+{
+    res *= a;
+    i += 1;
+}
+System.Console.WriteLine($"a= {a}; b= {b};");
+System.Console.WriteLine($"a^b =: {res} ");
---
+++ 29/29.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29 программа которая задает массив из 8 элементов и выводит их на экран
+//1,2,5,7,9,6,76,67 -> [1,2,5,7,9,6,76,67]
+
+/* Эту часть не допёр! 
+Console.Write("Введите целые числа через ", " :");
+string input = Console.ReadLine()!;
+int [] num;
+int accum = 0;
+
+num = Array.ConvertAll(input.Split(","), int.Parse);
+
+for (int i = 0; i<num.Length; i++)
+    accum ^=num[i];
+    Console.WriteLine($"Строка: {accum}");*/
+
+int [] nums = new int[8];
+int i = 0;
+while (i < nums.Length)
+{
+Console.Write($"Введите {i}-ое число :");
+nums [i]= int.Parse(Console.ReadLine()!);
+i ++;
+}
+for (i = 0; i<nums.Length; i++)
+{
+    Console.Write($"{nums[i]}  ");
+}
+
+
+
