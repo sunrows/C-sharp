2e75b1802a409f3fd6d3364b1ddb69e1c40e28d1
sunrowsforus <sunrowsforus@mail.ru>
2022-11-24 00:47:15 +0300

    Нечестные задачи 41-43

---
+++ 41/Program.cs
@@ -0,0 +1,42 @@
+﻿// Считать сколько чисел больше 0 ввел пользователь! 
+
+Console.WriteLine(" Введите числа:");
+
+void massive(int[] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.WriteLine(" Введите числа с Enter!");
+        array[i] = int.Parse(Console.ReadLine()!);
+        i++;
+    }
+}
+
+string ConsoleArr(int [] mass)
+{
+    return "{" + string.Join(",", mass) + "}";
+}
+
+int sumOfNumber(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0)
+        {
+            sum++;
+        
+        }
+    }
+    return sum;
+}
+
+Console.WriteLine(" Введите длину массива");
+int numberOfElements = int.Parse(Console.ReadLine()!);
+int [] getArray = new int [numberOfElements];
+massive(getArray);
+int Sum = sumOfNumber(getArray);
+Console.WriteLine(ConsoleArr(getArray));
+
+Console.WriteLine($" Количество положительных чисел: {Sum}");
---
+++ 43/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая найдёт точку
+// пересечения двух прямых, заданных уравнениями y = k1 *
+// x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+// пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 =
+// 9 -> (-0,5; -0,5)
+
+int[] Pounds()
+{
+    int[] mas = new int[4];
+    string[] arr = { "b1", "k1", "b2", "k2" };
+    for (int i = 0; i < mas.Length; i++)
+    {
+        
+        Console.WriteLine($" Введите точку {arr[i]}:\t");
+        mas[i] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(mas[i]);
+    }
+    return mas;
+}
+
+void Method(int[] array)
+{
+    int b1 = array[0];
+    int k1 = array[1];
+    int b2 = array[2];
+    int k2 = array[3];
+    double x, y;
+    if (b1 == b2 && k1 == k2) Console.WriteLine(" Прямые совпадают");
+    else if (k1 == k2) Console.WriteLine(" Прямые паралельны!");
+    else
+    {
+    x = (b2 - b1) / (k2 - k1);
+    y = k1 * x + b1;
+    Console.WriteLine($" Точка пересечения x = {x}; y = {y}");
+    }
+}
+
+Method(Pounds());
